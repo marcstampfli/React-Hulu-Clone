a8db80ae9e596050145d10149b6b5e39794710f2
marcstampfli <marcstampfli@gmail.com>
2020-09-14 23:01:02 -0400

    Initial

---
+++ src/Nav.js
@@ -0,0 +1,90 @@
+import React from "react";
+import "./Nav.css";
+import requests from "./requests";
+
+function Nav({ setSelectedOption }) {
+  return (
+    <div className="nav">
+      <h2
+        onClick={() => {
+          setSelectedOption(requests.fetchTrending);
+        }}
+      >
+        Trending
+      </h2>
+      <h2
+        onClick={() => {
+          setSelectedOption(requests.fetchTopRated);
+        }}
+      >
+        {" "}
+        Top Rated
+      </h2>
+      <h2
+        onClick={() => {
+          setSelectedOption(requests.fetchAction);
+        }}
+      >
+        Action
+      </h2>
+      <h2
+        onClick={() => {
+          setSelectedOption(requests.fetchComedy);
+        }}
+      >
+        Comedy
+      </h2>
+      <h2
+        onClick={() => {
+          setSelectedOption(requests.fetchHorror);
+        }}
+      >
+        Horror
+      </h2>
+      <h2
+        onClick={() => {
+          setSelectedOption(requests.fetchRomance);
+        }}
+      >
+        Romance
+      </h2>
+      <h2
+        onClick={() => {
+          setSelectedOption(requests.fetchMystery);
+        }}
+      >
+        Mystery
+      </h2>
+      <h2
+        onClick={() => {
+          setSelectedOption(requests.fetchSciFi);
+        }}
+      >
+        Sci-Fi
+      </h2>
+      <h2
+        onClick={() => {
+          setSelectedOption(requests.fetchWestern);
+        }}
+      >
+        Western
+      </h2>
+      <h2
+        onClick={() => {
+          setSelectedOption(requests.fetchAnimation);
+        }}
+      >
+        Animation
+      </h2>
+      <h2
+        onClick={() => {
+          setSelectedOption(requests.fetchTV);
+        }}
+      >
+        TV Movie
+      </h2>
+    </div>
+  );
+}
+
+export default Nav;
