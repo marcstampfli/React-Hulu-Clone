74ffc01e70a06693b68a9a4ec9df342224b3b811
marcstampfli <marcstampfli@gmail.com>
2020-09-14 23:15:33 -0400

    Updates

--- src/Nav.js
+++ src/Nav.js
@@ -5,84 +5,84 @@ import requests from "./requests";
 function Nav({ setSelectedOption }) {
   return (
     <div className="nav">
-      <h2
+      <h3
         onClick={() => {
           setSelectedOption(requests.fetchTrending);
         }}
       >
         Trending
-      </h2>
-      <h2
+      </h3>
+      <h3
         onClick={() => {
           setSelectedOption(requests.fetchTopRated);
         }}
       >
         {" "}
         Top Rated
-      </h2>
-      <h2
+      </h3>
+      <h3
         onClick={() => {
           setSelectedOption(requests.fetchAction);
         }}
       >
         Action
-      </h2>
-      <h2
+      </h3>
+      <h3
         onClick={() => {
           setSelectedOption(requests.fetchComedy);
         }}
       >
         Comedy
-      </h2>
-      <h2
+      </h3>
+      <h3
         onClick={() => {
           setSelectedOption(requests.fetchHorror);
         }}
       >
         Horror
-      </h2>
-      <h2
+      </h3>
+      <h3
         onClick={() => {
           setSelectedOption(requests.fetchRomance);
         }}
       >
         Romance
-      </h2>
-      <h2
+      </h3>
+      <h3
         onClick={() => {
           setSelectedOption(requests.fetchMystery);
         }}
       >
         Mystery
-      </h2>
-      <h2
+      </h3>
+      <h3
         onClick={() => {
           setSelectedOption(requests.fetchSciFi);
         }}
       >
         Sci-Fi
-      </h2>
-      <h2
+      </h3>
+      <h3
         onClick={() => {
           setSelectedOption(requests.fetchWestern);
         }}
       >
         Western
-      </h2>
-      <h2
+      </h3>
+      <h3
         onClick={() => {
           setSelectedOption(requests.fetchAnimation);
         }}
       >
         Animation
-      </h2>
-      <h2
+      </h3>
+      <h3
         onClick={() => {
           setSelectedOption(requests.fetchTV);
         }}
       >
         TV Movie
-      </h2>
+      </h3>
     </div>
   );
 }
